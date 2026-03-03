32c41068d56de2b6c4084fb7d99fe1a555e5c1cc
bullets012 <lancegenesismercado0111@gmail.com>
2026-03-03 15:32:12 +0800

    commmit

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+const slider = tns({
+  container: ".testimonialSlider",
+  items: 1,
+  slideBy: 1,
+  speed: 100,
+  autoplay: true,
+  navPosition: "bottom",
+  nav: true,
+  autoplayButtonOutput: false,
+  rewind: true,
+  controlsContainer: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
+});
